dca87f5bc69bc4863927bf828eb531e91c722ba1
AmokDev <amokdevv@gmail.com>
2024-05-05 10:11:11 +0300

    fridacompile module install frida fix

--- frida_compile.py
+++ frida_compile.py
@@ -13,7 +13,7 @@
 # © Copyright 2024 | https://t.me/AmokModules
 # Licensed under the MIT License
 # meta developer: @AmokModules
-# requires: frida-tools
+# requires: frida-tools frida
 
 import os
 import frida
